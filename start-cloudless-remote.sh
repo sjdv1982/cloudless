0dc9e801c69a4ebfa6b8bc8386daf422d996bcb3
Sjoerd de Vries <sjdv1982@gmail.com>
2020-07-11 13:31:58 +0200

    Cloudless remote is now working

--- start-cloudless-remote.sh
+++ start-cloudless-remote.sh
@@ -3,18 +3,22 @@ echo 'Starting up job slaves...'
 nslaves=$1
 : ${nslaves:=4}
 set -u -e
+dummy=$CLOUDLESS_NODES
 name_template=cloudless-jobslave
-nodes=$(echo $CLOUDLESS_NODES | sed 's/,/ /g')
+nodes=($(echo $CLOUDLESS_NODES | sed 's/,/ /g'))
 nodeIPs=''
 for node in $nodes; do
     nodeIP=$(ssh -G $node | awk '$1=="hostname" {print $2}')
     nodeIPs=$nodeIPs" "$nodeIP
 done
-nodeIPs="${nodeIPs:1}"
+nodeIPs=("${nodeIPs:1}")
 
 communion_incoming=''
-for nodeIP in $nodeIPs; do
-    communion_incoming2=$(ssh $nodeIP "bash -l -c 'cd $CLOUDLESSDIR && python3 scripts/jobslaves.py $nslaves $name_template --remote --ip $nodeIP --master-ip $masterIP'")
+for (( n=0; n<${#nodeIPs[@]}; n++ )); do
+    nodeIP="${nodeIPs[n]}"
+    node="${nodes[n]}"
+    echo $nodeIP $node
+    communion_incoming2=$(ssh $node "bash -l -c 'cd \$CLOUDLESSDIR && python3 scripts/jobslaves.py $nslaves $name_template --remote --ip $nodeIP --master-ip \$masterIP'")
     communion_incoming=$communion_incoming','$communion_incoming2
 done
 communion_incoming="${communion_incoming:1}"
@@ -22,9 +26,10 @@ communion_incoming="${communion_incoming:1}"
 echo "Job slaves are listening at " $communion_incoming
 export SEAMLESS_COMMUNION_INCOMING=$communion_incoming
 echo 'Starting up Cloudless web server...'
-###python3 scripts/cloudless.py cloudless-serve-graph-thin
+python3 scripts/cloudless.py cloudless-serve-graph-thin
 if [ $nslaves -gt 0 ]; then
-    for nodeIP in $nodeIPs; do
-        ssh $nodeIP "bash -l -c 'cd $CLOUDLESSDIR && ./kill-jobslaves.sh $nslaves $name_template'"
+    for node in $nodes; do
+        echo $node
+        ssh $node "bash -l -c 'cd \$CLOUDLESSDIR && ./kill-jobslaves.sh $nslaves $name_template'"
     done
 fi
