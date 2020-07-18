12b0867b284836f17bc5f42f96f5aa0c27fd82ab
Sjoerd de Vries <sjdv1982@gmail.com>
2020-07-18 22:51:53 +0200

    updated guide; now use status visualization from Cloudless, not from Seamless Docker image

--- init.sh
+++ init.sh
@@ -14,7 +14,11 @@ docker cp $container:/home/jovyan/seamless-tests/highlevel/share-pdb-docker.zip 
 docker cp $container:/home/jovyan/seamless-tests/highlevel/share-pdb-docker.seamless testgraph-docker.seamless
 $CLOUDLESSDIR/docker/commands/cloudless-add-zip testgraph-docker.zip
 
+# Run the following to update the status-visualization from the Seamless Docker image
 docker cp $container:/home/jovyan/software/seamless/graphs/status-visualization.zip status-visualization.zip
+docker cp $container:/home/jovyan/software/seamless/graphs/status-visualization.seamless status-visualization.seamless
+# /Run
+
 $CLOUDLESSDIR/docker/commands/cloudless-add-zip status-visualization.zip
 
 rm testgraph.zip testgraph-docker.zip status-visualization.zip
