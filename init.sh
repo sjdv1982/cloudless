1d62108c0feb5de2fc0d910f8201b944e9373340
Sjoerd de Vries <sjdv1982@gmail.com>
2022-06-26 15:49:13 +0200

    minor

--- init.sh
+++ init.sh
@@ -7,11 +7,11 @@ cd $CLOUDLESS_GRAPHS_DIR
 container=$(docker create rpbs/seamless)
 
 # Run the following to update the status-visualization from the Seamless Docker image
-docker cp $container:/home/jovyan/software/seamless/graphs/status-visualization.zip DEFAULT-status.zip
-docker cp $container:/home/jovyan/software/seamless/graphs/status-visualization.seamless DEFAULT-status.seamless
+docker cp $container:/home/jovyan/software/seamless/graphs/status-visualization.zip DEFAULT-webctx.zip
+docker cp $container:/home/jovyan/software/seamless/graphs/status-visualization.seamless DEFAULT-webctx.seamless
 # /Run
 
-seamless-add-zip DEFAULT-status.zip
+seamless-add-zip DEFAULT-webctx.zip
 
-rm DEFAULT-status.zip
+rm DEFAULT-webctx.zip
 docker rm $container
